ebb614dadaaed8bdd01bb026d7eebe5f9807d1e9
Shru-Singh <gshrutisingh@gmail.com>
2021-12-13 17:42:53 +0530

    what we do-done

--- components/what_we/what_we_do.js
+++ components/what_we/what_we_do.js
@@ -1,15 +1,19 @@
 import styles from "./what.module.scss";
 
 export default function What() {
- return (
-     <>
-     <div className={styles.whatWeDo}>
-             <div className={styles.head1}>WHAT WE DO</div>
-              <div className={styles.head2}>
-               WHAT WE DO
-              </div>
-     </div>
-     </>
- )
-
-};
+  return (
+    <>
+      <div className={styles.whatWeDo}>
+        <div className={styles.col1}>
+          <div className={styles.head1}>WHAT WE DO</div>
+          <div className={styles.head2}>WHAT WE DO</div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna Ut enim minim
+          veniam, quis nostrud exercitation ullamco laboris nisi
+        </div>
+      </div>
+    </>
+  );
+}
