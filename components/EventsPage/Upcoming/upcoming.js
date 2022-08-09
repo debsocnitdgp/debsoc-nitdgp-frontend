d28af9553d07c834499822f8af1100630ac87910
Aiman <aimanaisha851@gmail.com>
2022-08-09 12:26:50 +0530

    added logos

--- components/EventsPage/Upcoming/upcoming.js
+++ components/EventsPage/Upcoming/upcoming.js
@@ -12,7 +12,7 @@ export default function UpcomingEvents() {
     const events = upComing.upcoming
     setData(events);
   }, [url]);
-
+ 
   useEffect(() => {
     getData();
   }, [url, getData]);
