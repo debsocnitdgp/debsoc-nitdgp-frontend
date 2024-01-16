875f224102e9453cb434fdda32a25c8db1d5c481
Jayesh Toshniwal <jayeshvr1110@gmail.com>
2024-01-17 00:00:49 +0530

    Changes made for Reverberate to go live

--- components/reverberate.js
+++ components/reverberate.js
@@ -61,7 +61,27 @@ export default function Reverberate(props) {
               *Do not refresh or change the page while typing the answers.
             </div>
             <div className={style.inputGrp}>
-              <div className={style.question}>Name</div>
+              <div className={style.question}>Team Name</div>
+              {/* <div style={{ marginTop: 8, marginLeft: 8, fontWeight: 700 }}>
+                This is a team event. You can make a team of 3-5 people. Every
+                team member will have to register seperately, just keep the team
+                names same.
+              </div>
+              <div style={{ margin: 8, color: "#4caf50", fontWeight: 700 }}>
+                If you don&apos;t have a team, you can register individually as
+                well. We will get you a team later on.
+              </div> */}
+              <input
+                type="text"
+                value={data.teamname}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, teamname: e.target.value });
+                }}
+              />
+            </div>
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 1 Name</div>
               <input
                 type="text"
                 value={data.name}
@@ -93,17 +113,17 @@ export default function Reverberate(props) {
                 }}
               />
             </div>
-            <div className={style.inputGrp}>
+            {/* <div className={style.inputGrp}>
               <div className={style.question}>Section</div>
               <input
                 type="text"
-                value={data.section}
+                value={data.m1section}
                 placeholder="Enter your answer"
                 onChange={(e) => {
                   setData({ ...data, section: e.target.value });
                 }}
               />
-            </div>
+            </div> */}
             <div className={style.inputGrp}>
               <div className={style.question}>Hall No.</div>
               <input
@@ -115,6 +135,117 @@ export default function Reverberate(props) {
                 }}
               />
             </div>
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 2 Name</div>
+              <input
+                type="text"
+                value={data.name2}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, name2: e.target.value });
+                }}
+              />
+            </div>
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 2 Phone No</div>
+              <input
+                type="text"
+                value={data.number2}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, number2: e.target.value });
+                }}
+              />
+            </div>{" "}
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 2 Roll No.</div>
+              <input
+                type="text"
+                value={data.roll_no2}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, roll_no2: e.target.value });
+                }}
+              />
+            </div>
+            {/* <div className={style.inputGrp}>
+              <div className={style.question}>Member 2 Section</div>
+              <input
+                type="text"
+                value={data.section2}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, section2: e.target.value });
+                }}
+              />
+            </div> */}
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 2 Hall No.</div>
+              <input
+                type="text"
+                value={data.hallno2}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, hallno2: e.target.value });
+                }}
+              />
+            </div>
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 3 Name</div>
+              <input
+                type="text"
+                value={data.name3}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, name3: e.target.value });
+                }}
+              />
+            </div>
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 3 Phone No</div>
+              <input
+                type="text"
+                value={data.number3}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, number3: e.target.value });
+                }}
+              />
+            </div>{" "}
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 3 Roll No.</div>
+              <input
+                type="text"
+                value={data.roll_no3}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, roll_no3: e.target.value });
+                }}
+              />
+            </div>
+            {/* <div className={style.inputGrp}>
+              <div className={style.question}>Member 3 Section</div>
+              <input
+                type="text"
+                value={data.section3}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, section3: e.target.value });
+                }}
+              />
+            </div> */}
+            <div className={style.inputGrp}>
+              <div className={style.question}>Member 3 Hall No.</div>
+              <input
+                type="text"
+                value={data.hallno3}
+                placeholder="Enter your answer"
+                onChange={(e) => {
+                  setData({ ...data, hallno3: e.target.value });
+                }}
+              />
+            </div>
+
             {loading && (
               <div className={style.loading}>
                 Hold on! We are submitting your responses...
